bfbf9dd7fa90cd7f3254cc03ebd5bd44fd2fe3ad
Ranuga-D <76640227+Programmer-RD@users.noreply.github.com>
2021-01-14 09:37:47 +0530

    Update applications.py

--- applications.py
+++ applications.py
@@ -1,10 +1,7 @@
 from flask import *
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import BaggingRegressor
 from sklearn.ensemble import RandomForestRegressor
 import random
 
@@ -12,13 +9,7 @@ application = Flask(__name__)
 app = application
 @application.route("/", methods=["POST", "GET"])
 def home():
-    random_choice = random.choice([1, 2, 3])
-    if random_choice == 1:
-        model = pickle.load(open("./models/model_BaggingRegressor.pkl", "rb"))
-    elif random_choice == 2:
-        model = pickle.load(open("./models/model_KNeighborsRegressor.pkl", "rb"))
-    else:
-        model = pickle.load(open("./models/model_RandomForestRegressor.pkl", "rb"))
+    model = pickle.load(open("./models/model_RandomForestRegressor.pkl", "rb"))
     flash("Models Accuracy in testing is over 62.5% the accuracy may change", "success")
     flash('DataSet : https://www.kaggle.com/imdevskp/corona-virus-report | User : https://www.kaggle.com/imdevskp | Website : https://www.kaggle.com/','info')
     if request.method == "POST":
